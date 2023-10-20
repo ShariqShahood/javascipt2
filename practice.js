ef7e82fc1dfc1abbb134da97bc255158a8bd2e02
SHARIQ SHAHOOD <shariqshahood43@gmail.com>
2023-10-20 08:07:59 -0700

    Initial commit

---
+++ practice.js
@@ -0,0 +1,375 @@
+                    // CHAPTAER_05
+                // QUESTION:01    
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1 + num2 ;
+// console.log("Sum Of "+ num1 +" & "+num2+" is "+sum);
+                    // OR
+// var num1 = +prompt();
+// var num2 = +prompt();
+// var sum = num1 + num2 ;
+// console.log("Sum Of "+ num1 +" & "+num2+" is "+sum);
+               
+                // QUESTION:02
+// FOR SUBTRACTION
+// var num1 = +prompt();
+// var num2 = +prompt();
+// var sub = num1 - num2 ;
+// console.log("Sub Of "+ num1 +" & "+num2+" is "+sub);
+// FOR MULTIPLICATION    
+// var num1 = +prompt();
+// var num2 = +prompt();
+// var mul = num1 * num2 ;
+// console.log("MUL Of "+ num1 +" & "+num2+" is "+mul);
+// FOR DIVISION    
+// var num1 = +prompt();
+// var num2 = +prompt();
+// var Div = num1 / num2 ;
+// console.log("DIV Of "+ num1 +" & "+num2+" is "+Div);
+
+                // QUESTION:03
+
+// console.log("value after variable declaration is "+alert());
+// var num = 5;
+// console.log("Initial value : " + num++);
+// console.log("Value After Increament is : "+ num)
+// var sum = num + 7;
+// console.log("value after adiition is : " + sum--)
+// console.log("Value after decreament is : " + sum)
+// var Remainder = sum % 3 ;
+// console.log("The Remainder is : "+ Remainder)
+
+                // QUESTION:04
+
+// var cost = 600 ;
+// console.log("Cost For single Ticket Is : "+cost);
+// var noOfTickets = prompt("Enter Number Of Tickets You Want: " );
+// var totalCost = cost * noOfTickets;
+// console.log("Total Cost To Buy "+ noOfTickets + " Tickets to a movie  is " + totalCost +"PKR")
+
+                // QUESTION:05
+
+// var num = prompt("Enter Which Table you want: ");
+// console.log("Table Of "+ num);
+
+// for (let i=1; i<11; i++)
+// {
+// console.log(num + "x" + i + "=" + num*i)
+// }
+
+                // QUESTION:06
+
+// var select = prompt("Press 'A' for conversion Celcius To Farhenheit \n Press 'B' for conversion Farhenheit to Celcius.")
+
+// if( select == 'A' || select == 'a'){
+
+//     var Celcius = prompt("Enter Celcius : ")
+//     var ans = ((Celcius * 9/5)+32);
+//     console.log(Celcius + "'C is " + ans + "'F ")
+
+// }
+// else if(select == 'B' || select == 'b'){
+// var Farhenheit = prompt("Enter Farhenheit : ")
+// var ans = ((Farhenheit-32) * 5/9);
+// console.log(Farhenheit + "'F is " + ans + "'C")
+// }
+
+                // QUESTION:07
+
+// var item1 = 650 ;
+// var item2 = 100 ;
+// var qtn1 = prompt("Enter Quantity Of Item 1 : ");
+// var qtn2 = prompt("Enter Quantity Of Item 2 : ");
+// var shipcharges = 100 ;
+// console.log("Price Of Item 1 is "+ item1);
+// console.log("Quantity Of Item 1 is "+ qtn1);
+// console.log("Price Of Item 2 is "+ item2);
+// console.log("Quantity Of Item 2 is "+ qtn2);
+// console.log("Shipp[ing Charges is "+ shipcharges);
+// var cost = ((item1*qtn1)+(item2*qtn2)+shipcharges)
+// console.log("Total Cost Of Your Order is "+ cost);
+
+                // QUESTION:08
+
+// var tMarks = 980 ;
+// var markObt = +prompt("Enter Marks : ");
+// var per = ((markObt/ tMarks)*100);
+// console.log("Percentage : "+ per + "%");
+
+                // QUESTION:09
+
+// var doller = +prompt("Enter Currency in Doller: ");
+// var riyal = +prompt("Enter Currency in Riyal: ");
+// var totalCurrency = ((doller * 104.80) + (riyal * 28));
+// console.log("Total Currnecy in PKR : " + totalCurrency);
+
+                // QUESTION:10
+
+// var num = +prompt("Enter Number: ");
+// var ans = (((num+5)*10)/2);
+// console.log("Answer is : "+ ans);
+
+                // QUESTION:11
+
+// console.log("Age Calculator..");
+// var curYear = +prompt("Enter Current Year: ");
+// var birthYear = +prompt("Enter Birth Year: ");
+// var age = curYear - birthYear ; 
+// console.log("Your Age Is : "+ age);
+
+                // QUESTION:12
+
+// var r = prompt("Enter Radius : ");
+// var circum = 2 * 3.142 * r ;
+// var area = 3.142 * (r**2)
+// console.log("Circumference is : "+ circum);
+// console.log("Area Is : "+ area);
+
+                // QUESTION:13
+
+// var fSnack = prompt("Enter Favourite Snack: ");
+// var cAge = + prompt("Enter Age: ");
+// var estMaxAge = +prompt("Enter Max Est Age: ");
+// var amountOfSnack = +prompt("Enter amount of snacks per day: ");
+// var ans = ((estMaxAge-cAge) * amountOfSnack);
+// console.log("You will Need "+ ans +" "+ fSnack + " to last you until the ripe old age of "+estMaxAge);
+
+                    // CHAPTAER_06 to 09
+                // QUESTION:1
+
+// var a = +prompt("Enter Number : ");
+// console.log("The value of a is: "+a);
+// console.log("The Vale of ++a is :" + ++a)
+// console.log("Now the value of a is : "+ a);
+// console.log("The Vale of a++ is :" + a++);
+// console.log("The Vale of a is :" + a);
+// console.log("The Vale of --a is :" + --a);
+// console.log("The Vale of a is :" + a);
+// console.log("The Vale of a-- is :" + a--);
+// console.log("The Vale of a is :" + a);
+
+                // QUESTION:2
+
+// var a = 2;
+// var b = 1;
+// console.log("a is : "+a);
+// console.log("b is : "+ b);
+// var result = --a;
+// // console.log(" Pre Decremant :"+result);
+// var result = --a - --b;
+// // console.log("pre decreament both variable and then subtract :"+result)
+// var result = --a - --b + ++b;
+// // console.log("pre decreamnt both variable a & b  subtract and add with decreament b : "+result);
+// var result = --a - --b + ++b + b--;
+// // console.log("Final result: "+result)
+// console.log("Result is : "+ result);
+
+                // QUESTION:3
+// var name = prompt("Enter Name: ");
+// console.log("Hi! Welcome "+ name + " Thanks For Visiting Us....");
+
+                // QUESTION:6
+// var sub1 = prompt("Enter Subject 1 name: ");
+// var sub1Marks = +prompt("Enter Subject 1 Marks: ");
+// var sub2 = prompt("Enter Subject 2 name: ");
+// var sub2Marks = +prompt("Enter Subject 2 Marks: ");
+// var sub3 = prompt("Enter Subject 3 name: ");
+// var sub3Marks = +prompt("Enter Subject 3 Marks: ");
+// var Percentage1 = (sub1Marks / 100) * 100;
+// var Percentage2 = (sub2Marks / 100) * 100;
+// var Percentage3 = (sub3Marks / 100) * 100;
+// var sum = sub1Marks+sub2Marks+sub3Marks;
+// var Percentage = (sum / 300 )*100;
+// console.log("Subject Total MArks Obtained Marks Percentage ");
+// console.log(sub1 + "     " +"100" + "    " + sub1Marks + "         " + Percentage1+"%")
+// console.log(sub2 + "     " +"100" + "    " + sub2Marks + "         " + Percentage2+"%")
+// console.log(sub3 + "     " +"100" + "    " + sub3Marks + "         " + Percentage3+"%")
+// console.log("Total"  + "     " +"300" + "    " + sum + "         " + Percentage+"%")
+
+                    // CHAPTAER_9 to 11
+                // Question:01    
+// var city = prompt("Enter City Name: ");
+// if (city == "Karachi" || city == "karachi"){
+//     alert("Welcome To City Of Lights")
+// }
+// else if(city == "Islamabad" || city == "islamabad"){
+//     alert("Welcome To Capital Of Pakistan")
+// }
+// else{
+//     alert(city + " Is The City Of Pakistan")
+// }
+                // Question:02
+// var gender = prompt("Enter Gender: ");
+// if(gender == "Male" || gender == 'male' || gender == 'M' || gender== 'm'){
+//     alert("Good Morning Sir..");
+// }
+// else if(gender =="Female" || gender == 'female' || gender == 'F' || gender == 'f'){
+//     alert("Good Morning Maam...")
+// }
+
+                // Question:03
+// var color = prompt("Enter Signal Color: ");
+// if(color == "Red" || color == 'red'){
+//     alert("Must Stop..");
+    
+// }
+// else if(color =="Yellow" || color == 'yellow' ){
+//     alert("Ready To Move...")
+// }
+// else if(color =="Green" || color == 'green' ){
+//     alert("Move Now...")
+// }
+             // Question:04
+// var litre = prompt("Enter Remaining Fuel in Car : ");
+// console.log( "Your Current Fuel Is " +litre+ " Ltr");
+// if (litre < 0.25 ){
+//     alert("Your Current Fuel is " + litre + " Please Refill The Fuel In Your Car");
+// }
+             // Question:05
+        // (a)
+// var a=4;
+// if (++a == 5){
+//     alert("Given Condition for variable a is true");
+// }
+//         // (b)
+// var b = 82;
+// if (b++ == 83){
+//     alert("Given confition for variable b is true")
+// }
+//         // (c)
+// var c = 12;
+// if (c++ === 13){
+//     alert("condition 1 is true.");
+// }
+// else if (c === 13){
+//         alert("condition 2 is true.");
+// }
+// else if (++c < 14){
+//         alert("condition 3 is true.");
+// }
+// else if (c === 14){
+//         alert("condition 4 is true.");
+// }
+
+//         // (d)
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost)
+// {
+// alert("The cost equals");  
+// }
+
+//         // (e)
+// // if (true){
+// // alert("True");
+// // }
+// // if (false){
+// // alert("False");
+// // }
+//         // (f)\
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+             // Question:06
+// var sub1 = prompt("Enter Subject 1 name: ");
+// var sub1Marks = +prompt("Enter Subject 1 Marks: ");
+// var sub2 = prompt("Enter Subject 2 name: ");
+// var sub2Marks = +prompt("Enter Subject 2 Marks: ");
+// var sub3 = prompt("Enter Subject 3 name: ");
+// var sub3Marks = +prompt("Enter Subject 3 Marks: ");
+// var Percentage1 = (sub1Marks / 100) * 100;
+// var Percentage2 = (sub2Marks / 100) * 100;
+// var Percentage3 = (sub3Marks / 100) * 100;
+// var sum = sub1Marks+sub2Marks+sub3Marks;
+// var Percentage = (sum / 300 )*100;
+// console.log("Subject Total MArks Obtained Marks Percentage ");
+// console.log(sub1 + "     " +"100" + "    " + sub1Marks + "         " + Percentage1+"%")
+// console.log(sub2 + "     " +"100" + "    " + sub2Marks + "         " + Percentage2+"%")
+// console.log(sub3 + "     " +"100" + "    " + sub3Marks + "         " + Percentage3+"%")
+
+// if(Percentage >= 80){
+//     alert("Total Marks : 300 \n Marks Obtained : " + sum + " \n Percentage : "+ Percentage + "% \n Grade : A-one \n Remarks : Excellent");
+// }
+// else if ( Percentage >= 70){
+//        alert("Total Marks : 300 \n Marks Obtained : " + sum + " \n Percentage : "+ Percentage + "% \n Grade : A \n Remarks : Good");
+
+// }
+// else if ( Percentage >= 60){
+//         alert("Total Marks : 300 \n Marks Obtained : " + sum + " \n Percentage : "+ Percentage + "% \n Grade : B \n Remarks : You Need To Improve");
+
+// }
+// else if ( Percentage < 60){
+//         alert("Total Marks : 300 \n Marks Obtained : " + sum + " \n Percentage : "+ Percentage + "% \n Grade : Fail \n Remarks : Sorry ):");
+// }
+
+             // Question:07
+// var secretNum = 5 ; 
+// alert("Guessing Number Range 1 To 10");
+// var guessNum = +prompt();
+// if(guessNum == secretNum){
+//     alert("Bingo! Correct Answer")
+// }
+// else if(guessNum == secretNum + 1 || guessNum == secretNum - 1){
+//     alert("Close Enough to the correct answer")
+// }
+             // Question:08
+// var num = +prompt("Enter Number: ");
+// if (num % 3 == 0){
+// alert("This Number Is Divisible By 3")
+// }
+// else{
+//     alert("This Number Is not Divisble By 3")
+// }
+
+             // Question:09
+// var num = +prompt("Enter Number: ");
+// if (num % 2 == 0){
+// alert("This Number Is Even")
+// }
+// else{
+//     alert("This Number Is Odd")
+// }
+             // Question:10
+// var temp = prompt("Enter Temperature: ");
+// if(temp > 40 ){
+//     alert("It is too hot outside.")
+// }
+// else if(temp > 30 ){
+//     alert("The Weather today is Normal.")
+// }
+// else if(temp > 20 ){
+//     alert("Today's Weather is cool.")
+// }
+// else if(temp > 10 ){
+//     alert("OMG! Today's weather is so Cool.")
+// }
+// else if(temp <= 10 ){
+//     alert("OMG! It's Freezing.")
+// }
+
+             // Question:11
+var num1 = +prompt("Enter Number 1: ")
+var num2 = +prompt("Enter Number 2: ")
+var sum = num1 + num2;
+var sub = num1 - num2;
+var mul = num1 * num2;
+var div = num1 / num2;
+var per = (num1 / num2) * 100;
+var operator = prompt("Select Operator (+ , - , * , / , % ): ")
+if(operator == "+"){
+    alert("Sum Is : "+ sum)
+}
+else if (operator == "-"){
+    alert("Sub Is : "+ sub)
+}
+else if (operator == "*"){
+    alert("Mul Is : "+ mul)
+}
+else if (operator == "/"){
+    alert("Div Is : "+ div)
+}
+else if (operator == "%"){
+    alert("percentage Is : "+ per)
+}
